3dfc4020e6c6314cfc1f9df796f423116e3016dd
Zenobia Reeds <zreeds21@gmail.com>
2021-05-05 20:38:46 -0700

    fc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
@@ -10,14 +11,16 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="btn btn-danger"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <Button color="danger">Danger!</Button>
       </header>
+
     </div>
   );
 }
